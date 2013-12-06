898131993045b99670ab3e64a8b675f4f6089d5a
Alexis Gavoty <kload@kload.fr>
2013-12-06 23:14:31 +0100

    Update wp-config.php

--- conf/wp-config.php
+++ conf/wp-config.php
@@ -1,99 +1,28 @@
 <?php
-/**
- * La configuration de base de votre installation WordPress.
- *
- * Ce fichier contient les réglages de configuration suivants : réglages MySQL,
- * préfixe de table, clefs secrètes, langue utilisée, et ABSPATH.
- * Vous pouvez en savoir plus à leur sujet en allant sur 
- * {@link http://codex.wordpress.org/fr:Modifier_wp-config.php Modifier
- * wp-config.php}. C'est votre hébergeur qui doit vous donner vos
- * codes MySQL.
- *
- * Ce fichier est utilisé par le script de création de wp-config.php pendant
- * le processus d'installation. Vous n'avez pas à utiliser le site web, vous
- * pouvez simplement renommer ce fichier en "wp-config.php" et remplir les
- * valeurs.
- *
- * @package WordPress
- */
 
-// ** Réglages MySQL - Votre hébergeur doit vous fournir ces informations. ** //
-/** Nom de la base de données de WordPress. */
+// Database
 define('DB_NAME', 'yunobase');
-
-/** Utilisateur de la base de données MySQL. */
 define('DB_USER', 'yunouser');
-
-/** Mot de passe de la base de données MySQL. */
 define('DB_PASSWORD', 'yunopass');
-
-/** Adresse de l'hébergement MySQL. */
 define('DB_HOST', 'localhost');
-
-/** Jeu de caractères à utiliser par la base de données lors de la création des tables. */
 define('DB_CHARSET', 'utf8');
-
-/** Type de collation de la base de données. 
-  * N'y touchez que si vous savez ce que vous faites. 
-  */
 define('DB_COLLATE', '');
 
-/**#@+
- * Clefs uniques d'authentification et salage.
- *
- * Remplacez les valeurs par défaut par des phrases uniques !
- * Vous pouvez générer des phrases aléatoires en utilisant 
- * {@link https://api.wordpress.org/secret-key/1.1/salt/ le service de clefs secrètes de WordPress.org}.
- * Vous pouvez modifier ces phrases à n'importe quel moment, afin d'invalider tous les cookies existants.
- * Cela forcera également tous les utilisateurs à se reconnecter.
- *
- * @since 2.6.0
- */
-define('AUTH_KEY',         ' z+Oq8E~x.$J%(jdrTVJ?Jg|gxaE?f|sR<|V(t}]gQDW9yc}g<4u9buBvHyVo3;(');
-define('SECURE_AUTH_KEY',  'MGGqw@$4*dxNs%,~+V}~l8Cx3~F0b<]7Y-2&!%w];){Di>]P8)5%(S;4Jv;}5Hpi');
-define('LOGGED_IN_KEY',    'b(M|@_C*=RCx~~=X(-G|t{%3Up=h(Z|_UP&YA)]-d-js|VX3b1v1;NH[`#4L[EO=');
-define('NONCE_KEY',        'XseL2lp5}2_?~s&EJ_<f-gHvSN_E8A(4rz>Ut7{(VC-e4n$t_y(t}EpHKEy0`7<6');
-define('AUTH_SALT',        '+M|psJjNE(>kpozF-%$PN7y=q+E5PE?f-QBN[|.q0]m|n(6@Y<I,8GQCef=[P|^a');
-define('SECURE_AUTH_SALT', 'm<@Fkn,*`Ia$Bl#h~HAh =-%>o@h03YW3p~|*G5g5vYmoci[t$ 9EqLKS4-~;W>y');
-define('LOGGED_IN_SALT',   'jgQ4 ~ 0f6|zS}Cd6<&y7?+iRU`woOK65|M-_+X.[r Ka 6l}1fiH-m$dN2t)BcH');
-define('NONCE_SALT',       ';-FHEOgPbgx{{;Z19L7:v|f+9j-T5mv)g.L6-6_5KH|{-|p?G<}^rZ>$1qI/!wRb');
-/**#@-*/
+// Keys
+KEYSTOCHANGE
 
-/**
- * Préfixe de base de données pour les tables de WordPress.
- *
- * Vous pouvez installer plusieurs WordPress sur une seule base de données
- * si vous leur donnez chacune un préfixe unique. 
- * N'utilisez que des chiffres, des lettres non-accentuées, et des caractères soulignés!
- */
+// Prefix
 $table_prefix  = 'wp_';
 
-/**
- * Langue de localisation de WordPress, par défaut en Anglais.
- *
- * Modifiez cette valeur pour localiser WordPress. Un fichier MO correspondant
- * au langage choisi doit être installé dans le dossier wp-content/languages.
- * Par exemple, pour mettre en place une traduction française, mettez le fichier
- * fr_FR.mo dans wp-content/languages, et réglez l'option ci-dessous à "fr_FR".
- */
-define('WPLANG', 'en_EN');
+// i18n
+define('WPLANG', 'I18NTOCHANGE');
 
-/** 
- * Pour les développeurs : le mode deboguage de WordPress.
- * 
- * En passant la valeur suivante à "true", vous activez l'affichage des
- * notifications d'erreurs pendant votre essais.
- * Il est fortemment recommandé que les développeurs d'extensions et
- * de thèmes se servent de WP_DEBUG dans leur environnement de 
- * développement.
- */ 
+// Debug mode
 define('WP_DEBUG', false); 
 
-/* C'est tout, ne touchez pas à ce qui suit ! Bon blogging ! */
-
-/** Chemin absolu vers le dossier de WordPress. */
+// Path
 if ( !defined('ABSPATH') )
 	define('ABSPATH', dirname(__FILE__) . '/');
 
-/** Réglage des variables de WordPress et de ses fichiers inclus. */
+// WordPress settings path
 require_once(ABSPATH . 'wp-settings.php');

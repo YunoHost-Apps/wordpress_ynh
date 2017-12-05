4cd2060c63de546633e40a0b8ddba83182a165ac
Maniack Crudelis <maniackc_dev@crudelis.fr>
2017-12-05 19:46:40 +0100

    Update package

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -33,3 +33,19 @@ CHECK_SIZE () {	# Vérifie avant chaque backup que l'espace est suffisant
 		ynh_die "Espace nécessaire: $(HUMAN_SIZE $backup_size)"
 	fi
 }
+
+
+#=================================================
+#============= FUTURE YUNOHOST HELPER ============
+#=================================================
+
+# Delete a file checksum from the app settings
+#
+# $app should be defined when calling this helper
+#
+# usage: ynh_remove_file_checksum file
+# | arg: file - The file for which the checksum will be deleted
+ynh_delete_file_checksum () {
+	local checksum_setting_name=checksum_${1//[\/ ]/_}	# Replace all '/' and ' ' by '_'
+	ynh_app_setting_delete $app $checksum_setting_name
+}
